5161da0dbc193cced8e72ab4458985f87a5d731c
niki <nikishinde20@gmail.com>
2025-12-23 11:46:53 +0530

    American guy commit

--- src/qaclickacademy/RESTAPITest.java
+++ src/qaclickacademy/RESTAPITest.java
@@ -12,6 +12,12 @@ public class RESTAPITest {
 		System.out.println("postJira3");
 		
 		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		
+		
 	}
 	
 	
---
+++ src/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		System.out.println("WebAutomation");
+	}
+	
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+}
